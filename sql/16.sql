fec3f15c00bb08a1197e35e882e6e5583308515c
Advik Mareedu <amareedu@g.hmc.edu>
2026-03-08 16:36:40 -0700

    finished

--- sql/16.sql
+++ sql/16.sql
@@ -9,3 +9,21 @@
  * You can find examples of how to use the `rank` function at
  * <https://www.postgresqltutorial.com/postgresql-window-function/postgresql-rank-function/>.
  */
+
+SELECT 
+    RANK() OVER (ORDER BY filtered_rev.revenue DESC) as rank,
+    filtered_rev.title,
+    filtered_rev.revenue
+FROM (
+    SELECT film.title,COALESCE(revs.revenue,0.00) as revenue 
+    FROM film
+    LEFT JOIN (
+        SELECT film.title, SUM(payment.amount) as revenue
+        FROM rental
+        JOIN inventory USING (inventory_id)
+        JOIN film USING (film_id)
+        JOIN payment USING(rental_id)
+        GROUP BY film.title
+    ) as revs ON  revs.title = film.title
+) as filtered_rev
+ORDER BY rank ASC, filtered_rev.title ASC; 
--- sql/17.sql
+++ sql/17.sql
@@ -8,3 +8,29 @@
  * You might find the following stackoverflow answer useful for figuring out the syntax:
  * <https://stackoverflow.com/a/5700744>.
  */
+
+SELECT
+    og_ranks.rank,
+    og_ranks.title,
+    og_ranks.revenue,
+    sum(og_ranks.revenue) OVER (ORDER BY og_ranks.rank) AS "total revenue"
+FROM (
+    SELECT
+    RANK() OVER (ORDER BY filtered_rev.revenue DESC) as rank,
+    filtered_rev.title,
+    filtered_rev.revenue
+    FROM (
+        SELECT film.title,COALESCE(revs.revenue,0.00) as revenue
+        FROM film
+        LEFT JOIN (
+            SELECT film.title, SUM(payment.amount) as revenue
+            FROM rental
+            JOIN inventory USING (inventory_id)
+            JOIN film USING (film_id)
+            JOIN payment USING(rental_id)
+            GROUP BY film.title
+        ) as revs ON  revs.title = film.title
+    ) as filtered_rev
+    ORDER BY rank ASC, filtered_rev.title ASC
+) as og_ranks
+ORDER BY og_ranks.rank ASC, og_ranks.title ASC;
